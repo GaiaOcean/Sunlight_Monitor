7ad069f7173d9b3b22efd737bd13a340685ac337
Grazielle Almeida <86306468+GaiaOcean@users.noreply.github.com>
2025-04-12 10:32:53 -0300

    Add files via upload

---
+++ arduino/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+def read_sensor_data():
+    try:
+        with open('sensor_data.json', 'r') as file:
+            data = json.load(file)
+        return data.get("solar", 0), data.get("humidity", 0)
+    except FileNotFoundError:
+        return 0, 0
+
+@app.route('/')
+def index():
+    solar, humidity = read_sensor_data()
+    return render_template('index.html', solar=solar, humidity=humidity)
+
+if __name__ == '__main__':
+    app.run(debug=True)
